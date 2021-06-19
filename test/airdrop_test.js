8144ed0d38b6349e1ec582fb5a1b201664f71f34
fahraday <ghosterinc@gmail.com>
2021-06-19 10:35:34 -0400

    passing test, but still some problems

--- test/airdrop_test.js
+++ test/airdrop_test.js
@@ -3,25 +3,35 @@ const { expect } = require("chai");
 describe("Airdrop", async () => {
 
 
-  before(async function () {
-    this.Airdrop= await ethers.getContractFactory("Airdrop");
+  // before(async function () {
+  //   this.Airdrop = await ethers.getContractFactory("Airdrop");
+  //   accounts = await ethers.getSigners();
+  //   KevKen = await ethers.getContractFactory("KevKen");
+  //   kevken = await KevKen.deploy();
+
+  //   await kevken.deployed();
+
+  // });
+
+  // beforeEach(async function () {
+  //   this.airdropContract = await this.Airdrop.deploy(kevken.address, accounts[0].address);
+  //   await this.airdropContract.deployed();
+
+  // });
+
+  it("Admin initialize correctly", async () => {
+    this.Airdrop = await ethers.getContractFactory("Airdrop");
     accounts = await ethers.getSigners();
-     KevKen = await ethers.getContractFactory("KevKen");
-     kevken = await KevKen.deploy();
+    KevKen = await ethers.getContractFactory("KevKen");
+    kevken = await KevKen.deploy();
 
     await kevken.deployed();
 
-  });
-
-  beforeEach(async function () {
-    this.airdropContract = await this.Airdrop.deploy(kevken.address,accounts[0].address);
+    this.airdropContract = await this.Airdrop.deploy(kevken.address, accounts[0].address);
     await this.airdropContract.deployed();
 
-  });
 
-  it("Admin initialize correctly", async () => {
-
-    expect(await this.airdropContract.admin()).to.equal(accounts[0])
+    expect(await this.airdropContract.admin()).to.equal(accounts[0].address)
 
   });
 });
